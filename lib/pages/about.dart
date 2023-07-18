5546e4522cf7dabdf5c52a11246230fc44cea4e9
Cem Eren Badur <82811515+cemerenb@users.noreply.github.com>
2023-07-18 03:51:28 +0300

    Some bug fixes

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -1,8 +1,8 @@
-import 'package:pirim_depo/utils/text.dart';
 import 'package:flutter/material.dart';
+import 'package:pirim_depo/utils/text.dart';
 
 class About extends StatelessWidget {
-  const About({super.key});
+  const About({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -10,40 +10,40 @@ class About extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Hakkında'),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  aboutText1,
-                  style: const TextStyle(
-                      fontSize: 20, fontWeight: FontWeight.w500),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                aboutText1,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
                 ),
               ),
-              textBox(aboutText2),
-              textBox(aboutText3),
-              textBox(aboutText4),
-              textBox(aboutText5),
-              textBox(aboutText6),
-              textBox(aboutText7),
-              const Padding(
-                padding: EdgeInsets.all(20.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [Text('2023 @cemerenb')],
-                ),
-              )
-            ]),
-          )
-        ],
+            ),
+            textBox(aboutText2),
+            textBox(aboutText3),
+            textBox(aboutText4),
+            textBox(aboutText5),
+            textBox(aboutText6),
+            textBox(aboutText7),
+            const Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [Text('2023 @cemerenb')],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 
-  Padding textBox(var text) {
+  Padding textBox(String text) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Text(
